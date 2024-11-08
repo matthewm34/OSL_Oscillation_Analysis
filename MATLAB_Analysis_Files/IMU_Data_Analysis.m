56f31d89546c8926ebdfa112fecc880ba9ffd606
Matthew <mjmckenna34@gmail.com>
2024-11-07 23:27:38 -0500

    Updated Plotting Script for determining Frequency in Each Accelerometer X,Y,Z

--- MATLAB_Analysis_Files/IMU_Data_Analysis.m
+++ MATLAB_Analysis_Files/IMU_Data_Analysis.m
@@ -2,16 +2,17 @@
 % Date: November 6, 2024
 % 
 % NOTE: Make sure to run from the OSL_Oscillation_Analysis folder
-clear all; close all;
+clear all; close all; clc
+fprintf("Begin Plotting Script... \n");
 
 parent_folder_name = 'Converted_CSV_Data_Collection_1';
 
 folders_struct = dir(parent_folder_name);
-for i = 3:length(folders_struct) %loop through folders
+for i = 3:length(folders_struct) % loop through folders
     cur_folder_name = folders_struct(i).name;
     filename_struct = dir([parent_folder_name '/' cur_folder_name]);
 
-    for j = 3:length(filename_struct) %loop through files
+    for j = 3:length(filename_struct) % loop through files
         cur_filename = filename_struct(j).name;
 
         % Read data as a table
@@ -24,13 +25,41 @@ for i = 3:length(folders_struct) %loop through folders
         knee_theta = dataTable.knee_theta;
         knee_thetadot = dataTable.knee_thetadot;
     
-        % Plot Sensor Channel
+        % Plot entire signal trial as segmented by the bag2csv.py code
+        % figure();
+        % plot([0:1:length(foot_accelX)-1],foot_accelX)
+
+        % Plot only residual oscillations
+        %   Note: segment signal for residual oscillations between: 100 < data <300
         figure();
-        plot([0:1:length(foot_accelY)-1],foot_accelY)
-        xlabel('Digital Time Steps (k)'); ylabel('foot_accelY');
-        title([cur_filename]);
-        test = 0;
+        timesteps_mask = 100:300;                               % mask for residual oscillations
+        time_trimmed_ms = [0:1:length(timesteps_mask)-1] * 10 ; % time steps are sampled every 10 ms
+        time_trimmed_s = time_trimmed_ms * 1/1000;              
+        signal_trimmed = foot_accelY(timesteps_mask);           % choose which signal to plot
+        signal_trimmed = detrend(signal_trimmed);               % detrend the data, get rid of y axis offset
+        plot(time_trimmed_s,signal_trimmed)
+        xlabel('Time (s)'); ylabel('foot_accelY');
+        title(['Residual Oscillations: ', cur_filename], 'Interpreter', 'none');
+
+        %compute Fast Fourier Transform (FFT) to Find the Frequency of Oscillation
+        Yf = fft(signal_trimmed);
+        % Define the frequency axis
+        N = length(Yf);  % Number of data points
+        dt = .01;       % Sampling interval s
+        df = 1/(N*dt);  % Frequency resolution
+        nf = floor(N/2)+1;
+        f = (0:nf-1)'*df;
+        Yf = Yf(1:nf);
+        acceleration_magnitude = abs(Yf); % Compute the magnitude of the FFT (convert to acceleration)
+   
+        figure();
+        plot(f,acceleration_magnitude) % plot FFT
+        title([cur_filename, "  Acceleration vs. Frequency"], 'Interpreter', 'none');
+        xlabel("Frequency (Hz)")
+        ylabel("Acceleration (m/s)")
     end
-    printf('Press any key to plot next data set');pause; % analyze plots wait until keypress to continue
-    close all;
+    fprintf('Plotted all files in: %s \n', cur_folder_name);
+    fprintf("PAUSED: Press any key close all plots and generate the next set of plots \n");
+    pause;      
+    close all;  % close all trial plots for this set of k and b params
 end
