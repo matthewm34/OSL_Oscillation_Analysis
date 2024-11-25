edc299c90863804661bada8a732428204758c3fd
Matthew <mjmckenna34@gmail.com>
2024-11-25 18:01:08 -0500

    updated file names

--- ZV_Shaper.py
+++ ZV_Shaper.py
@@ -1,40 +1,33 @@
-# Code Outline for Convolving ZVD Shaper in real time
+# Code Outline for Convolving ZV Shaper in real time
     # designed to shape the torque only during the swing phase of the OSL
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Example torque curve (torque_vec) and timesteps_vec (don't need these for real time implimentation, only used for simulation)
-x = np.arange(0,256,1)      
-torque_vec = np.concatenate((np.ones(len(x)), np.zeros(50)))  # pretend the torque vec is unit step function
-timesteps_vec = np.arange(0,306,1)
-
+x = np.arange(0,100,1)                           
+torque_vec = np.concatenate((np.ones(len(x)), np.zeros(50))) # pretend the torque vec is unit step function
+timesteps_vec = np.arange(0,150,1)
 
 # Convolve function shaping in real time
-def convolve_EI(cur_torque,time_step):
+def convolve(cur_torque,time_step):
     A_1 = cur_torque * Amplitude_1
-    buffer_1[time_delay1 + time_step]= cur_torque * Amplitude_2
-    A_2 = buffer_1[time_step]
-    buffer_2[time_delay2 + time_step]= cur_torque * Amplitude_3
-    A_3 = buffer_2[time_step]
+    buffer[time_delay + time_step]= cur_torque * Amplitude_2
+    A_2 = buffer[time_step]
 
-    torque_output = A_1 + A_2 + A_3 #output the torque
+    torque_output = A_1 + A_2 #output the torque
 
     return torque_output
     
 
-# Initialize our ZVD Shaper Parameters
-Amplitude_1 = .25
+# Initialize our ZV Shaper Parameters
+Amplitude_1 = .5
 Amplitude_2 = .5
-Amplitude_3 = .25
-time_delay1 = 9  
-time_delay2 = 19
+time_delay = 9  
 
 # Convolution initialization parameters
 time_step = 0                   # first time step of the swing phase should be indexed as 0
-buffer_1 = np.zeros(1000)         # make sure buffer is at least of length: max(time_step) + (time_delay)  
-buffer_2 = np.zeros(1000) 
+buffer = np.zeros(1000)         # make sure buffer is at least of length: max(time_step) + (time_delay)  
 shaped_torque_output_vec = []   # only for plotting 
 
 
@@ -43,7 +36,7 @@ shaped_torque_output_vec = []   # only for plotting
 for cur_torque in torque_vec:
 
     # in OSL code check if (in swing phase) -> then run this code and shape torque
-    torque_output = convolve_EI(cur_torque,time_step)   # calculated the shaped torque
+    torque_output = convolve(cur_torque,time_step)   # calculated the shaped torque
     time_step = time_step + 1                        # increase time_step
     shaped_torque_output_vec.append(torque_output)   # only for plotting 
 
@@ -51,8 +44,7 @@ for cur_torque in torque_vec:
 # plotting shaped torque result 
 plt.figure(1)
 plt.plot(timesteps_vec, shaped_torque_output_vec)
-plt.title('Shaped torque')
+plt.title('Shaped Torque')
 plt.xlabel('Time Steps')
 plt.draw()
 plt.show()
-
